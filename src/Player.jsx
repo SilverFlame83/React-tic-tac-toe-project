66e5aaf0a1343dbfbb38ca88d03a2f923b5bf79c
VelvetSyntax <imren_83@abv.bg>
2024-01-14 21:53:35 +0200

    state updating

--- src/Player.jsx
+++ src/Player.jsx
@@ -4,21 +4,24 @@ export default function ({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing(!isEditing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
+  //let clickBtn = "Edit";
 
   if (isEditing) {
     playerName = <input type="text" required />;
+    //clickBtn = "Save"
   }
+
   return (
     <li>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Edit" : "Save"}</button>
     </li>
   );
 }
