1c119940f5e2771e12766c1437fa5f9267129ad7
VelvetSyntax <imren_83@abv.bg>
2024-01-22 13:52:18 +0200

    deriving state

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import Player from "./Player";
 import GameBoard from "./GameBoard";
 import Log from "./Log";
 
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = "X";
+
+  if (gameTurns.length >0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
 export default function App() {
-  const [activePlayer, setActivePlayer] = useState("X");
+  //const [activePlayer, setActivePlayer] = useState("X");
   const [gameTurns, setGameTurns] = useState([]);
 
-  function handleActivePlayer(rowIndex, colIndex) {
-    setActivePlayer((currentPlayer) => (currentPlayer === "X" ? "O" : "X"));
-    setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
+  const activePlayer = deriveActivePlayer(gameTurns)
 
-      if (prevTurns.length >0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+  function handleActivePlayer(rowIndex, colIndex) {
+    //setActivePlayer((currentPlayer) => (currentPlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => {
+     const currentPlayer = deriveActivePlayer(prevTurns)
 
       const updatedTurns = [
         { square: { row: rowIndex, col: colIndex }, player: currentPlayer},
